65a969109b88cc1bae69ade4996d468a616b5ce6
elnaz famkar <ellefamkar@gmail.com>
2022-12-02 12:46:39 +0330

    feat : add GSAP library to the project

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const h1s = document.querySelectorAll('h1');
-
-const letters = new SplitText(h1s).chars;
-
-[...h1s].forEach((h1) => {
-	h1.style.display = 'block'
-});
-
-const to = gsap.from(letters, {
-	y: 100,
-	rotation: 10,
-	duration: 2,
-	stagger: 0.1,
-	ease: "power3.inOut"
-});
-
-document.getElementById('restart').addEventListener('click', () => {
-	to.restart();
-});
+gsap.from(".title", {
+    duration: 3,
+    scale: 2, 
+    opacity: 0, 
+    delay: 0.5, 
+    stagger: 0.3,
+    ease: "elastic", 
+    force3D: true
+  });
