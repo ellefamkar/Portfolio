8bd5bcc1fe1c78ef1550efcb86e97f000e3da5e7
elnaz famkar <ellefamkar@gmail.com>
2022-12-02 15:44:55 +0330

    style : add styles and animation for home title

--- js/app.js
+++ js/app.js
@@ -13,12 +13,44 @@
 //     }
 // });
 
+
+
 gsap.from(".title", {
-    duration: 3,
+    duration: 5,
     scale: 0.9, 
     opacity: 0, 
-    delay: 0.2, 
+    delay: 0.02, 
+    // yPercent: 50,
     stagger: 0.3,
-    ease: "elastic", 
+    // ease: "elastic", 
     force3D: true
   });
+
+let tl = gsap.timeline();
+
+tl.from("h1 span", {
+  duration: 0.75,
+  y: 150,
+  scale: 0.9, 
+  autoAlpha: 0,
+  ease: Power3.out,
+  stagger: 1.5
+}).from("li", {
+  duration: 0.75,
+  x: 300,
+  autoAlpha: 0,
+  ease: "elastic.out(1, 1)",
+  stagger: {
+    each: 0.75,
+    amount: 0.5
+  }
+}, "+=0.25");
+
+
+// const myText = document.querySelector(".title-one");
+// gsap.to(".char",{
+//     y:0,
+//     stagger: 0.05,
+//     delay: 0.2,
+//     duration: 0.1
+// })
