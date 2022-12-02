05970475af46bb7aa8ee9ddcdc57a2d70c481470
elnaz famkar <ellefamkar@gmail.com>
2022-12-02 14:53:30 +0330

    feat : add GSAP libraries

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 // gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin( Draggable , MotionPathPlugin, TextPlugin);
+
+// Draggable.create("#draggable", {
+//     type:"x",
+//     bounds: document.getElementById("title"),
+//     inertia: true,
+//     onClick: function() {
+//          console.log("clicked");
+//     },
+//     onDragEnd: function() {
+//          console.log("drag is complete");
+//     }
+// });
 
 gsap.from(".title", {
     duration: 3,
-    scale: 2, 
+    scale: 0.9, 
     opacity: 0, 
-    delay: 0.5, 
+    delay: 0.2, 
     stagger: 0.3,
     ease: "elastic", 
     force3D: true
