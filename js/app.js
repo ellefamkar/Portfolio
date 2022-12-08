f5caa0d6b0f25911610b8b8f4c243cb2c338794c
elnaz famkar <ellefamkar@gmail.com>
2022-12-08 09:56:29 +0330

    feat : add description paragraph and modify its style

--- js/app.js
+++ js/app.js
@@ -137,4 +137,20 @@ observer.observe(target);
 
 
 // ------------------------
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".js-panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container-slider",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
 
