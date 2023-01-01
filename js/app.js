e4f04969e94c4dc41ae5b20444489e1169d6e3a9
elnaz famkar <86571329+ellefamkar@users.noreply.github.com>
2023-01-01 12:36:13 +0330

    delete : remove extra js files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // cursor 
+// import { gsap, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ, TweenLite, TimelineLite, TimelineMax } from "./gsap-core.js";
 
 // require.config({
 //   paths: {
